cb3a878ce817a4b5ecf7ac3adce73a707cdb7c72
Jordan Miller <Jordan.kay@gmail.com>
2024-01-05 14:39:49 -0700

    git push

--- apps/serve/lib/services/platform.dart
+++ apps/serve/lib/services/platform.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 
+/// we always check web first because Platform is not supported on web and will
+/// throw an exception if we try to use it while running on web.
 bool isWeb() => kIsWeb;
 bool isAndroid() => kIsWeb ? false : (Platform.isAndroid ? true : false);
 bool isIOS() => kIsWeb ? false : (Platform.isIOS ? true : false);
